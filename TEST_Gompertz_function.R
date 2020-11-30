0fcb1b379ba9347798025cbf2c0b021fb5bb0fd7
emilygsimmonds <emilygsimmonds@gmail.com>
2020-11-30 10:54:01 +0000

    Added extra test for Gompertz function

--- TEST_Gompertz_function.R
+++ TEST_Gompertz_function.R
@@ -6,6 +6,8 @@
 
 source("Gompertz_function.R")
 
+###############################################################################
+
 #### Test 1: does it work? ####
 
 # set sensible parameter values
@@ -21,23 +23,29 @@ r <- c(1, 1.25)
 N <- c(3.3, 4.6)
 e <- c(0.1, 0.5)
 alpha_ionj <- -0.1
-alpha_joni <- -0.1
-alphas <- matrix(c(0.7, -0.1, -0.1, 0.7), 
+alpha_joni <- 0.1
+
+# matrix format = 
+# aii, aionj,
+# ajoni, ajj
+
+alphas <- matrix(c(0.7, -0.1, 0.1, 0.7), 
                  ncol = 2,
                  byrow = TRUE) # diagonal = intra-specific effects!!
 
-Gompertz_func(r = r, N = N, e = e, alphas = alphas)
+## TEST
 
-# manual check:
+if(Gompertz_func(r = r, N = N, e = e, alphas = alphas)[1] ==
+   (r[1] + (alphas[1,1]*N[1]) + (alpha_joni*N[2]) + e[1])){print("YAY")}
 
-r[1] + (alphas[1,1]*N[1]) + (alpha_joni*N[2]) + e[1]
-r[2] + (alphas[2,2]*N[2]) + (alpha_ionj*N[1]) + e[2]
 
-### YAY #### it works!
+if(Gompertz_func(r = r, N = N, e = e, alphas = alphas)[2] ==
+   (r[2] + (alphas[2,2]*N[2]) + (alpha_ionj*N[1]) + e[2])){print("double YAY")}
+
 
 ###############################################################################
 
-#### Does it give the right error? ####
+#### Test 2: Does it give the right error? ####
 
 # set r to NULL
 
@@ -90,4 +98,40 @@ Gompertz_func(r = r, N = N, e = e, alphas = alphas) # Yes
 N <- 1
 
 Gompertz_func(r = r, N = N, e = e, alphas = alphas) # Yes
+
+###############################################################################
+
+#### Test 3: does the colSums() part work? ####
+
+# set sensible parameter values
+
+# arguments are: r (intrinsic growth rates), N (abundances), e (noise), 
+# alphas (interactions)
+
+# r, N, e are all vectors with length = number of species
+# alphas = matrix with ncol = nrow = number of species
+
+# set r and e to 0 to test just the interactions part
+r <- c(0, 0)
+N <- c(3.3, 4.6)
+e <- c(0, 0)
+alpha_ionj <- -0.1
+alpha_joni <- 0.1
+
+# matrix format = 
+# aii, aionj,
+# ajoni, ajj
+
+alphas <- matrix(c(0.7, -0.1, 0.1, 0.7), 
+                 ncol = 2,
+                 byrow = TRUE) # diagonal = intra-specific effects!!
+
+## TEST
+
+if(Gompertz_func(r = r, N = N, e = e, alphas = alphas)[1] ==
+   (r[1] + (alphas[1,1]*N[1]) + (alpha_joni*N[2]) + e[1])){print("YAY")}
+
+
+if(Gompertz_func(r = r, N = N, e = e, alphas = alphas)[2] ==
+   (r[2] + (alphas[2,2]*N[2]) + (alpha_ionj*N[1]) + e[2])){print("double YAY")}
 
